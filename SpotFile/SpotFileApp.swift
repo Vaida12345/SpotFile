4353a9f78262905630dcac2dec0cb14b46317612
Vaida <developer@vaidaf.net>
2024-02-04 09:53:05 +0800

    Initial Commit

---
+++ SpotFile/SpotFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotFileApp.swift
+//  SpotFile
+//
+//  Created by Vaida on 2024/2/4.
+//
+
+import SwiftUI
+
+@main
+struct SpotFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
